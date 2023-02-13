123c47521f26fb1c7be243d0de168855b5399267
konovod <konovod@gmail.com>
2023-02-13 10:47:42 +0300

    better spec infrastructure

--- spec/nonoecs-template_spec.cr
+++ spec/nonoecs-template_spec.cr
@@ -1,5 +1,19 @@
 require "./spec_helper"
 
+Spec.before_suite do
+  systems = ECS::Systems.new(SPEC_WORLD)
+    .add(BasicSystems.new(SPEC_WORLD))
+    .add(GameSystems.new(SPEC_WORLD))
+
+  systems.init
+  systems.execute
+  Engine.log("Starting spec suite...")
+end
+
+Spec.after_suite do
+  Engine.log("Spec suite complete")
+end
+
 describe "app" do
   # TODO: Write tests
 
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,4 @@
 require "spec"
 require "../src/app"
+
+SPEC_WORLD = ECS::World.new
